e712af07154291a778d18d3c1ac9988f227ab580
PravinkumarZopsmart <zop8603@of060g23n1md6nk.dlink>
2021-11-29 18:23:11 +0530

    Created method to return number of resume screening

--- src/main/java/com/automate_hiring_port/pages/Dashboard.java
+++ src/main/java/com/automate_hiring_port/pages/Dashboard.java
@@ -9,13 +9,25 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class Dashboard {
-    private static final By numberOfApplications = By.cssSelector(".number-of-total-candidates-text");
     private static final int waitSeconds = 5;
+    private static final By numberOfApplications = By.cssSelector(".number-of-total-candidates-text");
+    private static final By resumeScreeningCount = By.className("MuiBadge-badge MuiBadge-anchorOriginTopRightRectangle");
 
     public static int getNumberOfApplications(WebDriver driver) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(waitSeconds));
         wait.until(ExpectedConditions.visibilityOfElementLocated(numberOfApplications));
         WebElement applications = driver.findElement(numberOfApplications);
         return Integer.parseInt(applications.getText());
+    }
+
+    public static int getNumberOfResumeScreening(WebDriver driver) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(waitSeconds));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(resumeScreeningCount));
+            return Integer.parseInt(driver.findElement(resumeScreeningCount).getText());
+        } catch (Exception e) {
+            return 0;
+        }
+
     }
 }
