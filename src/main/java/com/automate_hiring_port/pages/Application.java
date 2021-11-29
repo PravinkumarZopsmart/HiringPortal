3c423df3c3d0bfb8c55e3d809f405712d59264ac
PravinkumarZopsmart <zop8603@of060g23n1md6nk.dlink>
2021-11-29 15:26:03 +0530

    Created method to return number of applications in the table

--- src/main/java/com/automate_hiring_port/pages/Application.java
+++ src/main/java/com/automate_hiring_port/pages/Application.java
@@ -10,10 +10,22 @@ import java.time.Duration;
 
 public class Application extends BaseClass {
     private static final By pageHeadingLocator =By.cssSelector(".head-text-applications-list");
+    private static final By tableContentsLocator = By.cssSelector(".MuiTableBody-root tr");
 
     public static String getPageHeading(WebDriver driver) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
         wait.until(ExpectedConditions.visibilityOfElementLocated(pageHeadingLocator));
         return driver.findElement(pageHeadingLocator).getText();
+    }
+
+    public static int getNumberOfApplications(WebDriver driver) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(tableContentsLocator));
+            return driver.findElements(tableContentsLocator).size();
+        } catch (Exception e) {
+            System.out.println(e.getClass().getName());
+            return 0;
+        }
     }
 }
